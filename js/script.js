0df6fadd632d9870b2e4d9b3aba56e6d772e9a83
Ant <ant@moped.com>
2013-07-10 17:22:08 +0200

    initial style, playlist

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
+var playlist = [
+  {
+  	'artist':'MJ Cole',
+  	'title':'Sincere',
+  	'filename':'mjcole-sincere.mp3'
+  },
+  {
+  	'artist':'Boards of Canada',
+  	'title':'Cold Earth',
+  	'filename':'boardsofcanada-coldearth.mp3'
+  },
+  {
+  	'artist':'Cloud 9',
+  	'title':'Do You Want Me Baby? (Dusky Remix)',
+  	'filename':'cloud9-doyouwantmebaby-duskyremix.mp3'
+  },
+];
+
+console.log(playlist[0].artist);
+
 $(function() {
-  $( 'audio' ).audioPlayer();
+  $( 'audio' ).audioPlayer({
+  	classPrefix: 'playa',
+
+  	strPlay: '&#9654;',
+  	strPause: '&#8214;'
+  });
 });
